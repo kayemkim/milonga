335376914d0f36d298b0159b2de26b31b8ba7f7e
kmkim <kayemkim@gmail.com>
2013-07-15 19:25:29 +0900

    move javascript directory and delete old files.

--- js/atmos/atmos.js
+++
@@ -1,6 +0,0 @@
-var AtmosRequestMappingInfo = com.km.milonga.rhino.AtmosRequestMappingInfo;
-
-exports.define = function(url, handler) {
-	var armi = AtmosRequestMappingInfo.getInstance();
-	armi.put(url, handler);
-};
