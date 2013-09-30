8d7b33bb563aa00133874253fa76cb9b5d4a15b2
kmkim <kayemkim@gmail.com>
2013-09-30 15:15:50 +0900

    scalate template removed

--- src/main/java/package.scala
+++
@@ -1,8 +0,0 @@
-import org.mozilla.javascript.NativeObject
-import org.mozilla.javascript.IdScriptableObject
-
-package object Atmos {
-  
-  type Json = java.util.Map[String, NativeObject]
-  
-}
--- src/main/java/scala/map/Cheese.scala
+++
@@ -1,8 +0,0 @@
-package scala.map
-
-import org.fusesource.scalate.RenderContext.capture
-
-object Cheese {
-  def foo(productId: Int)(body : => Unit) =
-    <a href={"products/" + productId} title="Product link">capture(body)</a>
-}
